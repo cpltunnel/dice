dc82cbb3864abe1bd76deb403376f1dda5a14be4
Jeremy Golden <58534260+cpltunnel@users.noreply.github.com>
2019-12-06 14:40:11 -0600

    Cleanup
    Removed unnecessary parseint line.

--- dice.js
+++ dice.js
@@ -1,7 +1,5 @@
 let diceSides = prompt("Enter the number of sides");
 
-diceSides = parseInt (diceSides);
-
 if (diceSides != null && diceSides > 0){
 	alert("Result is " + Math.floor(Math.random() * diceSides + 1));
 }
